e81e87ae6d13f250f165b81ededd8b97520e3878
JOAQUIN RODAL CHORRO <informatico.joaquin@gmail.com>
2024-02-25 17:11:08 +0000

    Create sumar_array_js.js

---
+++ sumar_array_js.js
@@ -0,0 +1,8 @@
+Forma de sumar todos los elementos de un array
+lista = [1,2,3]
+lista.reduce((a,b) => a + b , 0 ) 
+
+Otra forma de sumar de un array es esta ...
+Math.sum(...lista)
+
+es bueno saber estas cosas ...
